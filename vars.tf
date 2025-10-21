440178c023d09d576c84bb78a9038afa1d5fcda4
Raman Khanna <89792896+ramannkhanna2@users.noreply.github.com>
2025-10-21 13:51:21 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,12 +1,12 @@
 variable "region" {
   description = "The AWS region to launch instances in"
   type        = string
-  default     = "us-west-2"  # Modify this with your preferred AWS region
+  default     = "us-east-2"  # Modify this with your preferred AWS region
 }
 
 variable "key_name" {
   description = "The SSH key pair name to access the EC2 instances"
   type        = string
-  default     = "raman-oregon-key"  # Modify this with your SSH key pair
+  default     = "raman-ohio-key"  # Modify this with your SSH key pair
 }
 
