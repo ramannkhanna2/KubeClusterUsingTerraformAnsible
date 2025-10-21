2fe01fd14657e00f67553400840c5b8d9b339378
Raman Khanna <89792896+ramannkhanna2@users.noreply.github.com>
2025-10-21 13:45:24 +0530

    Update default region and key name in vars.tf

--- vars.tf
+++ vars.tf
@@ -1,12 +1,12 @@
 variable "region" {
   description = "The AWS region to launch instances in"
   type        = string
-  default     = "sa-east-1"  # Modify this with your preferred AWS region
+  default     = "us-west-2"  # Modify this with your preferred AWS region
 }
 
 variable "key_name" {
   description = "The SSH key pair name to access the EC2 instances"
   type        = string
-  default     = "raman-sao-key"  # Modify this with your SSH key pair
+  default     = "raman-oregon-key"  # Modify this with your SSH key pair
 }
 
